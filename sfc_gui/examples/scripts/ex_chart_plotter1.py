74238854c917873594a15ca8419e78e25b363ce8
Brian Romanchuk <brianr747@gmail.com>
2017-03-16 08:24:04 -0400

    Tweaks to the example.

--- sfc_gui/examples/scripts/ex_chart_plotter1.py
+++ sfc_gui/examples/scripts/ex_chart_plotter1.py
@@ -1,8 +1,8 @@
 # coding=utf-8
-# import sfc_models.gl_book.chapter3 as chapter3
 from sfc_models.objects import *
 from sfc_gui.chart_plotter import ChartPlotterWindow2
 
+register_standard_logs('output', __file__)
 mod = Model()
 # Create first country - Canada. (This model only has one country.)
 can = Country(mod, 'Canada', 'CA')
@@ -15,10 +15,12 @@ tax = TaxFlow(can, 'TaxFlow', 'TF', .2)
 labour = Market(can, 'Labour market', 'LAB')
 goods = Market(can, 'Goods market', 'GOOD')
 # Need to set the exogenous variable - Government demand for Goods ("G" in economist symbology)
-mod.AddExogenous('GOV', 'DEM_GOOD', '([20.,] * 5) + [30.,]*100 ')
+mod.AddExogenous('GOV', 'DEM_GOOD', '([20.,] * 40) + [30.,]*100 ')
 mod.AddGlobalEquation('t', 'Decorated Time Axis', '1950.0 + 0.25*k')
+# Create a decorative variable
+mod.AddGlobalEquation('G', 'Government Consumption', 'GOV__DEM_GOOD')
 
-mod.EquationSolver.TraceStep = 7
+mod.EquationSolver.TraceStep = 47
 mod.EquationSolver.SolveInitialSteadyState = True
 mod.main()
 
